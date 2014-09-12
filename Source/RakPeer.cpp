48f4a064c5db9f194664d565075682cf4dda19d5
David Staessens <nobody@noone.com>
2014-09-12 12:11:37 +0200

    Fixed GetTimeoutTime function in RakPeer.cpp not working.

--- Source/RakPeer.cpp
+++ Source/RakPeer.cpp
@@ -2559,7 +2559,7 @@ RakNet::TimeMS RakPeer::GetTimeoutTime( const SystemAddress target )
 		RemoteSystemStruct * remoteSystem = GetRemoteSystemFromSystemAddress( target, false, true );
 
 		if ( remoteSystem != 0 )
-			remoteSystem->reliabilityLayer.GetTimeoutTime();
+			return remoteSystem->reliabilityLayer.GetTimeoutTime();
 	}
 	return defaultTimeoutTime;
 }
