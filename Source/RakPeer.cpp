1fa2bc5b44e9390929553de247481825d77292a6
Jason Gauci <jgmath2000@gmail.com>
2015-01-06 00:51:41 -0800

    Bug handling timestamps with different endian-ness

--- Source/RakPeer.cpp
+++ Source/RakPeer.cpp
@@ -3778,6 +3778,7 @@ void RakPeer::ShiftIncomingTimestamp( unsigned char *data, const SystemAddress &
 #endif
 
 	RakNet::BitStream timeBS( data, sizeof(RakNet::Time), false);
+        timeBS.EndianSwapBytes(0,sizeof(RakNet::Time));
 	RakNet::Time encodedTimestamp;
 	timeBS.Read(encodedTimestamp);
 
